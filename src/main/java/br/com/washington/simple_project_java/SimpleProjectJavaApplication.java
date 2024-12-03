593d323d9e94c913441a29aba5592c5d42b349a3
Washington <washington195luiz@gmail.com>
2024-12-03 12:40:44 -0300

    First Commit

---
+++ src/main/java/br/com/washington/simple_project_java/SimpleProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.simple_project_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjectJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/washington/simple_project_java/SimpleProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.simple_project_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
